9747a726e67df06256f3bdb0b21c96c2ee8a511f
Vellah Naswa <92290865+GaiusLyrics@users.noreply.github.com>
2021-10-19 21:09:02 +0300

    Add files via upload
    How to come up with a simple calculator

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("LIBRARY SERVICES\n");
+
+    printf("What would you like to do\n");
+
+    printf("1.Sign up\n");
+
+    printf("2.Sign in\n");
+
+    printf("3.Add Book in your shelf\n");
+
+    printf("4.Purchase a book");
+
+    return 0;
+}
